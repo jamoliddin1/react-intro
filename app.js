49b37515d2468acc079de3a9a3cc2090f77d02a3
jamoliddin1 <chinaliyevj@gmail.com>
2022-05-30 18:50:27 +0500

    added jsx and fix err

--- app.js
+++ app.js
@@ -1,36 +1,17 @@
-import React from "react";
 import { render } from "react-dom";
-import carComponent from "./car";
+import CarComponent from "./car";
 
-// One specific type of car in factory
 const AppReact = () => {
-  return React.createElement("div", {}, [
-    React.createElement(
-      "h1",
-      { class: "that-how__give-class" },
-      "My first React App"
-    ),
-
-    // Stamping the stamp 3 times
-    React.createElement(carComponent, {
-      name: "Lamborghini",
-      type: "SuperCar",
-      color: "Gold",
-    }),
-    React.createElement(carComponent, {
-      name: "Tesla",
-      type: "ElectroCar",
-      color: "Red",
-    }),
-    React.createElement(carComponent, {
-      name: "Honda",
-      type: "Civic",
-      color: "Grey",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me</h1>
+      <CarComponent name="Lamborghini" type="SuperCar" color="Gold" />
+      <CarComponent name="Tesla" type="ElectroCar" color="Red" />
+      <CarComponent name="Honda" type="Civic" color="Grey" />
+    </div>
+  );
 };
 
 // Producing car from a factory (poping it out )
-const producingByOne = React.createElement(AppReact);
 
-render(producingByOne, document.getElementById("root"));
+render(<AppReact />, document.getElementById("root"));
--- car.js
+++ car.js
@@ -1,11 +1,11 @@
-import React from "react";
-
-const carComponent = (propers) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, propers.name),
-    React.createElement("h2", {}, propers.type),
-    React.createElement("h2", {}, propers.color),
-  ]);
+const CarComponent = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.type}</h2>
+      <h2>{props.color}</h2>
+    </div>
+  );
 };
 
-export default carComponent;
+export default CarComponent;
