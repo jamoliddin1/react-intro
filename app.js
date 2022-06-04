9ba54b987b3afa9ec4744fd39768387729d3f904
jamoliddin1 <chinaliyevj@gmail.com>
2022-06-04 19:12:43 +0500

    useState and add form

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 import { render } from "react-dom";
-import CarComponent from "./car";
-
+import SearchParams from "./searchParams";
 const AppReact = () => {
   return (
     <div>
       <h1>Adopt Me</h1>
-      <CarComponent name="Lamborghini" type="SuperCar" color="Gold" />
-      <CarComponent name="Tesla" type="ElectroCar" color="Red" />
-      <CarComponent name="Honda" type="Civic" color="Grey" />
+      <SearchParams />
     </div>
   );
 };
---
+++ searchParams.js
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+const Animals = ["dog", "cat", "rabbit", "bird", "fish"];
+
+const SearchParams = () => {
+  // const location = "New York USA";
+  const [location, setLocation] = useState("");
+  // console.log(location);
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {Animals.map((arrAnimal) => (
+              <option key={arrAnimal} value={arrAnimal}>
+                {arrAnimal}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+            onBlur={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((arrBreed) => (
+              <option key={arrBreed} value={arrBreed}>
+                {arrBreed}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
